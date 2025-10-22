020788b94258a44c9a1482feba062dd0d184584f
Andre-Franco1 <andrefranco3172@gmail.com>
2025-10-21 21:54:51 -0300

    feat: developing product endpoints

---
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -0,0 +1,35 @@
+package com.abutua.product_backend.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.abutua.product_backend.models.Product;
+
+@RestController
+public class ProductController {
+
+    private List<Product> products = Arrays.asList(new Product(1, "Product 1", 100.50),
+            new Product(2, "Product 2", 200.50),
+            new Product(3, "Product 3", 300.50));
+
+    @GetMapping("products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        Product prod = products.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+        return ResponseEntity.ok(prod);
+    }
+
+    @GetMapping("products")
+    public List<Product> getProducts() {
+        return products;
+    }
+}
