f89d00786ae9585985e0612f952e0ab69721bfae
Andre-Franco1 <andrefranco3172@gmail.com>
2025-10-28 22:05:24 -0300

    feat: complete integration between frontend and backend

--- product-backend/src/main/java/com/abutua/product_backend/resources/CategoryController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,14 @@ import org.springframework.web.server.ResponseStatusException;
 import com.abutua.product_backend.models.Category;
 
 @RestController
+@CrossOrigin
 public class CategoryController {
     
     private List<Category> categories = Arrays.asList(
         new Category(1, "Category 1"),
         new Category(2, "Category 2"),
-        new Category(3, "Category 3"));
+        new Category(3, "Category 3"),
+        new Category(4, "Category 4"));
         
     @GetMapping("categories/{id}")
     public ResponseEntity<Category> getCategory(@PathVariable int id) {
